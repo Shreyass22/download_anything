9af642a6f185d52bd6ba2eb367614ea5674149d3
Shreyas Jaiswal <47319276+Shreyass22@users.noreply.github.com>
2020-11-16 15:33:49 +0530

    final

--- app/src/main/java/com/example/downloadanything/MainActivity.java
+++ app/src/main/java/com/example/downloadanything/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.downloadanything;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.app.DownloadManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -19,13 +25,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_STORAGE_CODE = 1000;
 
     EditText urlEt;
     Button downloadUrl;
-    private ProgressDialog progressDialog;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +72,103 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            NotificationChannel channel = new NotificationChannel("Notification", "Notification", NotificationManager.IMPORTANCE_DEFAULT);
+//            NotificationManager manager = getSystemService(NotificationManager.class);
+//            manager.createNotificationChannel(channel);
+//        }
+//
+//        downloadUrl.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+//                } else {
+//                    startDownloading();
+//                }
+//
+//            }
+//        });
+
     }
 
+    private class DownloadFile extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            int count;
+            try {
+                URL url = new URL(strings[0]);
+                URLConnection connection = url.openConnection();
+                connection.connect();
+                int filelen = connection.getContentLength();
+                String filepath = Environment.getExternalStorageDirectory().getAbsolutePath();
+                InputStream input = new BufferedInputStream(url.openStream());
+                OutputStream output = new FileOutputStream(filepath + "/image.jpg");
+
+                byte data[] = new byte[1024];
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    publishProgress(String.valueOf((int) (total * 100) / filelen));
+                    output.write(data, 0, count);
+                }
+                output.flush();
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+
+            progressDialog.setTitle("progress bar");
+            progressDialog.setMessage("Downloading......");
+            progressDialog.setIndeterminate(false);
+            progressDialog.setMax(100);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressDialog.show();
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            urlEt.setText("");
+            sendNotification();
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            progressDialog.setProgress(Integer.parseInt(values[0]));
+        }
+
+
+    }
+
+    private void sendNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "My noti")
+                .setSmallIcon(R.drawable.icon_notification)
+                .setContentTitle("New Notification")
+                .setContentText("Thank you for downloading.")
+                .setAutoCancel(true);
+
+        //NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //notificationManager.notify(0, builder.build());
+        NotificationManagerCompat managerCompat =  NotificationManagerCompat.from(MainActivity.this);
+        managerCompat.notify(0, builder.build());
+
+    }
 
     private void startDownloading() {
         // get url from edittext
@@ -79,6 +189,7 @@ public class MainActivity extends AppCompatActivity {
         //get download service and enque file
         DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
         manager.enqueue(request);
+        new DownloadFile().execute(url);
     }
 
     //handle permission result
