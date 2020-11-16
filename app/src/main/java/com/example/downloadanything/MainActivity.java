52a292229e0bfc321638a73897d3622893fce553
Shreyas Jaiswal <47319276+Shreyass22@users.noreply.github.com>
2020-11-16 15:45:48 +0530

    final

--- app/src/main/java/com/example/downloadanything/MainActivity.java
+++ app/src/main/java/com/example/downloadanything/MainActivity.java
@@ -139,9 +139,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "My noti")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "Notification")
                 .setSmallIcon(R.drawable.icon_notification)
-                .setContentTitle("New Notification")
+                .setContentTitle("Notification")
                 .setContentText("Thank you for downloading.")
                 .setAutoCancel(true);
 
